131f1409c3a61efc7b2da7e119f2e0cf34a00379
Salvatore Turboli <salvatore.turboli@gmail.com>
2021-09-27 10:06:43 +0200

    Initial Script

---
+++ README.md
@@ -0,0 +1,12 @@
+### Examtopic Script to retrive all questions exam
+
+## Why this repository 
+on 2 Days to launch aws exam, examtopic not working and i see this
+![alt text](https://i.ibb.co/JnDgGBr/examtopic.png)
+I'm using google dorks to retrive all answer and using google cache to view all anwser
+
+
+## How to use 
+- Create environment: `python3 -m venv env`
+- Set Source: `bash ./env/Scripts/activate`
+- Install requirements: `pip install -r requirments.txt`
---
+++ main.py
@@ -0,0 +1,21 @@
+from googlesearch import search
+
+"""
+    query  : query string that we want to search for.
+    tld    : tld stands for top level domain which means we want to search our result on google.com or google.in or some other domain.
+    lang   : lang stands for language.
+    num    : Number of results we want.
+    start  : First result to retrieve.
+    stop   : Last result to retrieve. Use None to keep searching forever.
+    pause  : Lapse to wait between HTTP requests. Lapse too short may cause Google to block your IP. Keeping significant lapse will make your program slow but its safe and better option.
+    Return : Generator (iterator) that yields found URLs. If the stop parameter is None the iterator will loop forever.
+"""
+def googleSearch(query):
+    cacheProvider = "http://webcache.googleusercontent.com/search?q=cache:"
+    for j in search(query, tld="co.in", num=150, stop=150, pause=2):
+        print(cacheProvider + j)
+
+
+
+if __name__ == '__main__':
+    googleSearch("site:examtopics.com intext:practitioner")
